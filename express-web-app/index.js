8d0f40cca5088329d5730dd0090014223a06cc41
Anuj Kumar <anujkuma@adobe.com>
2018-07-15 22:16:48 +0530

    08_Init

--- express-web-app/index.js
+++ express-web-app/index.js
@@ -1,8 +1,31 @@
 const express = require('express');
+const hbs = require('hbs');
 const app = express();
 
+app.set('view engine', 'hbs'); // set express related configurations. key -> value pairs
+
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-	res.send('Hello World');
+	res.render('home.hbs', {
+		titlePage: 'Home Page',
+		contentHeader: 'Home Page',
+		contentBody: 'Welcome to the Portfolio Website',
+		currentYear: new Date().getFullYear()
+	});
+});
+
+app.get('/help', (req, res) => {
+	res.render('help.html');
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		titlePage: 'About Page',
+		contentHeader: 'About Page',
+		contentBody: 'This is a Portfolio Website',
+		currentYear: new Date().getFullYear()
+	});
 });
 
 app.listen(3000, () => {
